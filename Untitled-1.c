35b899b56c6a9ee771670fdbf24994160d8a5b5f
MR.Feng <17839949345@163.com>
2022-11-04 13:14:30 +0800

    add 3 file

---
+++ Untitled-1.c
@@ -0,0 +1,39 @@
+#include"stdio.h"
+int main()
+{
+/*	int price=0;
+	printf("请输入金额（元）: ");
+	scanf("%d",&price);
+	
+	int change = 100-price;
+	int j;
+	
+    printf("找您%d元。\n,%d , %d ",change,j,price);
+    
+   /* int a = 0;
+    int b = 0;
+    scanf("price%d %d",&a,&b);
+    printf("%d %d",a,b);
+    return 0;*/
+    
+   /* int price =0;
+    int amount;
+   
+    printf("请输入金额（元）：");
+    scanf("%d",&price);
+    
+    printf("请输入票面：");
+    scanf("%d",&amount); 
+    int change = amount -price;
+    printf("找您%d元。\n",change);
+    return 0;*/
+    
+    int a;
+    int b;
+    int main;
+    printf("请输入两个整数：");
+    scanf("%d, %d",&a,&b); 
+    printf("%d + %d =%d\n",a,b,a+b);
+	return 0;    
+
+}
